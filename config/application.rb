b0838074188db3f19e52b067cb17bab6390ca089
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-05 18:44:03 +0000

    fix env config

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,5 @@
 require_relative 'boot'
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -16,6 +11,12 @@ module Bookers2Ver2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.paths.add 'lib', eager_load: true
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
