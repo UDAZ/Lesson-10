4d47222d06280b6478ddf211885f36d48a9286ec
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-05 18:35:50 +0000

    add env

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,10 @@
 require_relative 'boot'
-
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
