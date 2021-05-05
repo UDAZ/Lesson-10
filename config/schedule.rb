fcba3dc651143236a5520adc1faf0a9cbdf23be4
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-05 19:09:40 +0000

    fix timing

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 set :environment, rails_env
 set :output, 'log/cron.log'
-every 2.minute do
+every 1.day do
   begin
     runner "Batch::SendMail.send_mail"
   rescue => e
